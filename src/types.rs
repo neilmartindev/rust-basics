df02f2f3dabe8a59b8478f6f7ef4764d9c668ef9
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-12-01 14:02:27 +0000

    Variables section

---
+++ src/types.rs
@@ -0,0 +1,12 @@
+// Data types
+
+// Integers: u8, i8, u16, i16, u32, i32, u64, i64, u128, i128
+// Floats: f32, f64
+// Boolean (bool)
+// Characters (char)
+// Tuples
+// Arrays
+
+pub fn run () {
+        
+}
